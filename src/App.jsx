af65c38024a4cbb53eeaf98f2aba0e419d9be46b
Busheng Lou <busheng@Bushengs-MacBook-Pro.local>
2017-09-24 13:14:27 -0700

    load data from json & add calcualte

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <AppBar title="H1b numbers" />
+        <AppBar title="H1b numbers" showMenuIconButton={false}/>
         <Content/>
       </div>
     )
