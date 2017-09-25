a1f21184b54b976cc5f9ada1862914f7007b9be6
Busheng Lou <busheng@Bushengs-MacBook-Pro.local>
2017-09-24 17:06:53 -0700

    style fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import AppBar from 'material-ui/AppBar';
 import Content from './components/Content'
 
+const style={
+  backgroundColor: '#4da6ff',
+}
+
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <AppBar title="H1b numbers" showMenuIconButton={false}/>
+        <AppBar title="H1b numbers" showMenuIconButton={false} style={style}/>
         <Content/>
       </div>
     )
